4c662c67ac0c361ce0a4188d6e0a8bc6cbd03606
Влад Козлов <vkozlov15012005@gmail.com>
2025-05-02 20:05:30 +0300

    Добавьте файлы проекта.

---
+++ PartyHard/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using PartyHard.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PartyHard
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static PartyHardDbEntities _context;
+        public static PartyHardDbEntities GetContext()
+        {
+            if (_context == null)
+            {
+                _context = new PartyHardDbEntities();
+            }
+            return _context;
+        }
+    }
+}
---
+++ PartyHard/AppData/AuthorisationHelper.cs
@@ -0,0 +1,44 @@
+﻿using PartyHard;
+using PartyHard.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyHard.AppData
+{
+    internal class AuthorisationHelper
+    {
+        private static PartyHardDbEntities _context = App.GetContext();
+        public static Users selectedUser;
+
+        /// <summary>
+        /// Авторизует пользователя
+        /// </summary>
+        /// <param name="login"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static bool Authorise(string login, string password)
+        {
+            if (string.IsNullOrWhiteSpace(login) || string.IsNullOrWhiteSpace(password))
+            {
+                MessageBoxHelper.Error("Не все поля для ввода были заполнены.");
+                return false;
+            }
+
+            selectedUser = _context.Users.FirstOrDefault(user => user.Login == login && user.Password == password);
+
+            if (selectedUser != null)
+            {
+                return true;
+            }
+            else
+            {
+                MessageBoxHelper.Error("Неправильно введен логин или пароль.");
+                return false;
+            }
+        }
+    }
+}
